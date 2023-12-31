eb966404954da6cfae45a52b249ba491697c4167
wwweert123 <puachengxuan.fp@gmail.com>
2023-12-31 19:20:26 +0800

    Add frontend for creating new thread

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,22 @@ import ForumThreadService from "../services/ForumThreadService";
 import { IForumThread } from "../type/ForumThread";
 import React from "react";
 
+import { Button, DialogActions, DialogContent, DialogContentText, DialogTitle, Fab, TextField } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import Dialog from "@mui/material/Dialog";
+
 const Home: React.FC = () => {
     const [forumThreads, setForumThreads] = React.useState<IForumThread[]>([]);
+
+    const [openThreadForm, setOpenThreadForm] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpenThreadForm(true);
+    };
+
+    const handleClose = () => {
+        setOpenThreadForm(false);
+    };
 
     React.useEffect(() => {
         let mounted = true;
@@ -23,6 +37,41 @@ const Home: React.FC = () => {
             <h3>{"Welcome to Frederick's Forum! Feel Free to Browse around and participate in discussions."}</h3>
             <br />
             <ThreadList forumThreads={forumThreads} />
+            <Fab color="primary" aria-label="add" variant="extended" size="medium" onClick={handleClickOpen}>
+                <AddIcon />
+                Add new thread
+            </Fab>
+            <Dialog fullScreen open={openThreadForm} onClose={handleClose}>
+                <DialogTitle>Create a new thread!</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Talk about anything you want! Share with others whatever you want to share about!
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Title"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                    />
+                    <TextField
+                        multiline
+                        rows={10}
+                        margin="dense"
+                        id="name"
+                        label="Description"
+                        type="text"
+                        fullWidth
+                        variant="filled"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleClose}>Submit</Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 };
--- src/services/ForumThreadService.ts
+++ src/services/ForumThreadService.ts
@@ -5,8 +5,13 @@ const getAllThreads = () => {
     return http.get<Array<IForumThread>>("/forum_threads").then((response) => response.data);
 };
 
+const createNewThread = (data: IForumThread) => {
+    return http.post<IForumThread>("/forum_threads", data);
+};
+
 const ForumThreadService = {
     getAllThreads,
+    createNewThread,
 };
 
 export default ForumThreadService;
