6edb14f3ee87684d6e14b6ed4ea7a60b4ba471bd
wwweert123 <puachengxuan.fp@gmail.com>
2024-01-02 01:00:29 +0800

    Add new type for received data from backend

--- src/type/ForumThread.ts
+++ src/type/ForumThread.ts
@@ -1,6 +1,21 @@
-export interface IForumThread {
+export type IThreadInfo = {
     title: string;
     description: string;
     upvotes: number;
     [key: string]: unknown;
-}
+};
+
+export type IForumThread = {
+    id: number;
+    type: string;
+    attributes: IThreadInfo;
+    [key: string]: unknown;
+};
+
+export type GetAllThreadsResponse = {
+    data: IForumThread[];
+};
+
+export type CreateThreadResponse = {
+    data: IForumThread;
+};
