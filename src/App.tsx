8bb6c83628e05055d25678cbb536760deddf595c
wwweert123 <puachengxuan.fp@gmail.com>
2024-01-02 14:49:24 +0800

    Use template theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,15 @@ import Home from "./pages/Home";
 // import BasicThreadView from "./pages/BasicThreadView";
 // import StyledThreadView from "./pages/StyledThreadView";
 import ThreadView from "./pages/ThreadView";
+import ThemeProvider from "./theme";
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { blue, orange } from "@mui/material/colors";
-
-const theme = createTheme({
-    palette: {
-        primary: blue,
-        secondary: orange,
-    },
-});
 
 const App: React.FC = () => {
     return (
         <div className="App">
-            <ThemeProvider theme={theme}>
+            <ThemeProvider>
                 <BrowserRouter>
                     <Routes>
                         {/* <Route path="/thread/1" element={<BasicThreadView />} />
---
+++ src/theme/index.js
@@ -0,0 +1,32 @@
+import palette from "./palette";
+//import typography from "./typography";
+import PropTypes from "prop-types";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider as MUIThemeProvider, createTheme, StyledEngineProvider } from "@mui/material/styles";
+import { useMemo } from "react";
+import React from "react";
+
+ThemeProvider.propTypes = {
+    children: PropTypes.node,
+};
+
+export default function ThemeProvider({ children }) {
+    const themeOptions = useMemo(
+        () => ({
+            palette,
+            shape: { borderRadius: 6 },
+        }),
+        [],
+    );
+
+    const theme = createTheme(themeOptions);
+
+    return (
+        <StyledEngineProvider injectFirst>
+            <MUIThemeProvider theme={theme}>
+                <CssBaseline />
+                {children}
+            </MUIThemeProvider>
+        </StyledEngineProvider>
+    );
+}
